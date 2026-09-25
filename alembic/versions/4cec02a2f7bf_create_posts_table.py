cef237e6880e83371631c3b779c02f6b12d0fb3d
binay_sharma <binaysharma0064@gmail.com>
2026-09-25 18:07:32 +0545

    worked with db migration tool

---
+++ alembic/versions/4cec02a2f7bf_create_posts_table.py
@@ -0,0 +1,37 @@
+"""create posts table
+
+Revision ID: 4cec02a2f7bf
+Revises: 
+Create Date: 2026-09-25 16:34:21.879834
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4cec02a2f7bf'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table("posts",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("content", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    #undo the changes made in the upgrade function
+        # op.dropcascade_table("posts")
+    # op.drop_table("posts")
--- app/models.py
+++ app/models.py
@@ -23,3 +23,9 @@ class User(Base):
     password=Column(String, nullable=False)
     created_at=Column(DateTime(timezone=True),nullable=False, server_default=sqlalchemy.sql.expression.text('now()'))
     posts = relationship("Post", back_populates="owner")
+
+
+# class Vote(Base):
+#     __tablename__ = "votes"
+#     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+#     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
